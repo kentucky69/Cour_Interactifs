e5850db5d0cd0e831e2e8401aae2b11beff994e9
Ken Gassy <kengassy@MacBook-Pro-de-Ken.local>
2020-04-25 12:02:45 +0200

    Ajout des sliders

--- Interactifs/ViewController.swift
+++ Interactifs/ViewController.swift
@@ -17,6 +17,10 @@ class ViewController: UIViewController {
     @IBOutlet weak var stepperLbl: UILabel!
     @IBOutlet weak var segmented: UISegmentedControl!
     @IBOutlet weak var segmentedLbl: UILabel!
+    @IBOutlet weak var minSlide: UILabel!
+    @IBOutlet weak var maxSlide: UILabel!
+    @IBOutlet weak var slideLbl: UILabel!
+    @IBOutlet weak var slider: UISlider!
     
     
     override func viewDidLoad() {
@@ -30,6 +34,11 @@ class ViewController: UIViewController {
         segmented.selectedSegmentIndex = 1
         segmented.selectedSegmentTintColor = .systemPink
         updateSegmentedLabel()
+        
+        slider.maximumTrackTintColor = .systemGray
+        minSlide.text = "min \(slider.minimumValue)°"
+        maxSlide.text = "max \(slider.maximumValue)°"
+        updateSliderChanged()
         }
     
     // (1)
@@ -58,6 +67,12 @@ class ViewController: UIViewController {
         }
     }
     
+    func updateSliderChanged() {
+        let value = Int(slider.value)
+        slideLbl.text = "Valeur : \(value)°"
+        
+    }
+    
     /**=======ACTIONS=======*/
     @IBAction func switchChanger(_ sender: UISwitch) {
          updateSwitchLabel()
@@ -67,6 +82,9 @@ class ViewController: UIViewController {
     }
     @IBAction func segmentedChanged(_ sender: UISegmentedControl) {
         updateSegmentedLabel()
+    }
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        updateSliderChanged()
     }
     
 }
